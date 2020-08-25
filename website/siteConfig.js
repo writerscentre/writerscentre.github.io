5df69e645196819d8baa4ecc4b5d09d2624af8d5
Amiel <amiel@writerscentre.com.au>
2020-08-25 14:15:58 +0800

    Added settings and few more docs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'awc-docs',
+  projectName: 'writerscentre.github.io',
   organizationName: 'writerscentre',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
