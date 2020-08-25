d16d156941b1a3558b9eec7b399c35c9f02a917b
Amiel <amiel@writerscentre.com.au>
2020-08-25 14:22:40 +0800

    siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'AWC', // Title for your website.
   tagline: 'Documentation site for AWC',
   url: 'https://writerscentre.github.io', // Your website URL
-  baseUrl: '/awc-docs/', // Base URL for your project */
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
